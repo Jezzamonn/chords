b1be121413dade8bffad83262e30b9db6860487a
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-11-10 09:48:59 -0800

    Fade out the audio nicely :)

--- js/sound-controller.js
+++ js/sound-controller.js
@@ -3,7 +3,12 @@ import { sinEaseInOut, slurp, clamp } from "./util";
 
 const NOTE_LENGTH = 0.5;
 const NUM_NOTES = 5;
-const FADE_LENGTH = 0.5;
+const VISUAL_FADE_LENGTH = 0.5;
+
+// Audio wise
+const START_VOLUME = 0.5;
+const VOLUME_FADE_AMT = 0.4;
+const BOUNCE_FADE_AMT = 0.7;
 
 const WIDTH = 200;
 const HEIGHT = 100;
@@ -24,7 +29,8 @@ export default class SoundController {
             playNote({
                 audioContext: this.audioContext,
                 scaleNote: this.startNote + 2 * i,
-                delay: i * NOTE_LENGTH
+                delay: i * NOTE_LENGTH,
+                volume: START_VOLUME * Math.pow(VOLUME_FADE_AMT, i)
             });
         }
     }
@@ -34,7 +40,7 @@ export default class SoundController {
 	 */
 	update(dt) {
         this.animAmt += dt;
-        if (this.animAmt > (NUM_NOTES - 1) * NOTE_LENGTH + FADE_LENGTH) {
+        if (this.animAmt > (NUM_NOTES - 1) * NOTE_LENGTH + VISUAL_FADE_LENGTH) {
             this.done = true;
         }
 	}
@@ -45,14 +51,15 @@ export default class SoundController {
 	render(context) {
         let noteAmt = (this.animAmt / NOTE_LENGTH) % 1;
         let noteIndex = Math.floor(this.animAmt / NOTE_LENGTH);
-        let fadeAmt = clamp((this.animAmt - (NUM_NOTES - 1) * NOTE_LENGTH) / FADE_LENGTH, 0, 1);
-        context.globalAlpha = 1 - fadeAmt;
 
         const bounceAmt = 1 - (4 * noteAmt * (1 - noteAmt));
+        const bounceHeight = 2 * HEIGHT * Math.pow(BOUNCE_FADE_AMT, noteIndex)
 
         const x = slurp(-WIDTH, WIDTH, (noteIndex + noteAmt) / NUM_NOTES);
-        const y = slurp(-HEIGHT, HEIGHT, bounceAmt);
+        const y = slurp(HEIGHT - bounceHeight, HEIGHT, bounceAmt);
 
+        let fadeAmt = clamp((this.animAmt - (NUM_NOTES - 1) * NOTE_LENGTH) / VISUAL_FADE_LENGTH, 0, 1);
+        context.globalAlpha = 1 - fadeAmt;
         context.beginPath();
         context.fillStyle = 'blue';
         context.arc(x, y, 2, 0, 2 * Math.PI);
